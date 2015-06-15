2ba743fab000e3a5e4cbae8b45b713727510ae12
Timur Ka <timurkh@yandex.ru>
2015-06-15 23:39:04 +0300

    Getting location address from IntentService

---
+++ app/src/main/java/com/tinyadvisor/geoadvisor/Constants.java
@@ -0,0 +1,7 @@
+package com.tinyadvisor.geoadvisor;
+
+/**
+ * Created by tkhakimyanov on 14.06.2015.
+ */
+public class Constants {
+}
---
+++ app/src/main/java/com/tinyadvisor/geoadvisor/FetchAddressIntentService.java
@@ -0,0 +1,15 @@
+package com.tinyadvisor.geoadvisor;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by tkhakimyanov on 14.06.2015.
+ */
+public class FetchAddressIntentService extends IntentService {
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
